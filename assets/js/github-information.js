0d67174652e07939b7bfa1ec81e6e0246ed8fc5c
Elvira Tidemo <elvir@MacBook-Air.local>
2024-01-25 14:41:18 +0100

    Adjust some things in github-information.js

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -65,7 +65,7 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
-            } else if (errorResponse === 403) {
+            } else if (errorResponse.status === 403) {
                 var resetTime = new Date(errorResponse.getResponseHeader('X-RateLimit-Reset')*1000);
                 $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`)
             } else {
